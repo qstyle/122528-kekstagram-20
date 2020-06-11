7b3d23ea7c5e706f80b1889e0cdbde151c1dea1d
qstyle <ordin404@gmail.com>
2020-06-11 21:29:24 +0300

    fix(main.js)add render

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 // функция рандома
@@ -8,11 +9,10 @@ var generateRandom = function (randomMin, randomMax) {
   }
   return randomValue;
 };
-var comment = {};
 
-// генерация коментариев
-function generateComments() {
-  var randomComment = generateRandom(0, 5);
+// генерация сообщения коментария
+function generateMessage() {
+  var message = [];
   var messageComent = [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
@@ -21,52 +21,70 @@ function generateComments() {
     'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
+  for (var i = 0; i < generateRandom(1, 2); i++) {
+    var randomMessageIndex = generateRandom(0, 5);
+    message.push(messageComent[randomMessageIndex]);
+    message.join('');
+  }
+  return message;
+}
+
+// генерация коментариев
+
+function generateComments() {
+  var comments = [];
+  var comment = {};
+  var generateComment = generateRandom(0, 5);
   var nameComent = [
     'name1', 'name2', 'name3', 'name4', 'name5', 'name6'
   ];
   var randomAvatar = [1, 2, 3, 4, 5, 6];
   comment = {
-    avatar: 'img/avatar-' + randomAvatar[randomComment] + '.svg',
-    message: messageComent[randomComment],
-    name: nameComent[randomComment]
+    avatar: 'img/avatar-' + randomAvatar[generateComment] + '.svg',
+    message: generateMessage(),
+    name: nameComent[generateComment]
   };
-  return comment;
+  for (var i = 0; i < generateRandom(1, 6); i++) {
+    comments.push(comment);
+  }
+  return comments;
 }
-var blockPhoto = [];
 
 // гернерация фото
-var generateBlockPhoto = function () {
-  for (var i = 0; i < 25; i++) {
-    var comments = generateComments();
+var blockPhoto = [];
+function generateBlockPhoto(numberOfCopies) {
+  for (var i = 0; i < numberOfCopies; i++) {
     var blockPhotoObj = {
       url: 'photos/' + (i + 1) + '.jpg',
       description: 'blablabla',
       likes: generateRandom(15, 250),
-      commentsObj: comments
+      comments: generateComments()
     };
-    blockPhoto[i] = blockPhotoObj;
+    blockPhoto.push(blockPhotoObj);
   }
-  return (blockPhoto);
-};
-generateBlockPhoto();
+  return blockPhoto;
+
+}
+generateBlockPhoto(25);
 
 // создание блока фото
-var picturesContainer = document.querySelector('.pictures');
-var photoElementBlock = document.querySelector('#picture') .content.querySelector('.picture');
+function renderPhoto() {
+  var picturesContainer = document.querySelector('.pictures');
+  var photoElementBlock = document.querySelector('#picture') .content.querySelector('.picture');
+  function renderBlockPhoto(rednerBlock) {
+    var photoElementClone = photoElementBlock.cloneNode(true);
+    photoElementClone.querySelector('.picture__img').src = rednerBlock.url;
+    photoElementClone.querySelector('.picture__likes').textContent = rednerBlock.likes;
+    photoElementClone.querySelector('.picture__comments').textContent = rednerBlock.comments.length;
+    return photoElementClone;
+  }
 
-function rendrBlockPhoto(rednerBlock) {
-  var photoElementClone = photoElementBlock.cloneNode(true);
-  picturesContainer.appendChild(photoElementClone);
-  photoElementClone.querySelector('.picture__img').src = rednerBlock.url;
-  photoElementClone.querySelector('.picture__likes').textContent = rednerBlock.likes;
-  photoElementClone.querySelector('.picture__comments').textContent = rednerBlock.commentsObj.avatar + rednerBlock.commentsObj.message + rednerBlock.commentsObj.name;
-  return photoElementClone;
+  // рендер всех блоков
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < blockPhoto.length; i++) {
+    fragment.appendChild(renderBlockPhoto(blockPhoto[i]));
+  }
+  picturesContainer.appendChild(fragment);
 }
+renderPhoto();
 
-// рендер всех блоков
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < blockPhoto.length; i++) {
-  fragment.appendChild(rendrBlockPhoto(blockPhoto[i]));
-}
-
-picturesContainer.appendChild(fragment);
