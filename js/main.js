15bc99bfb9dd6689aeb170174614ab130170e9a9
qstyle <ordin404@gmail.com>
2020-06-28 14:04:12 +0300

    add modal function

--- js/main.js
+++ js/main.js
@@ -127,6 +127,7 @@ function openBigPicture(photoObject) {
 // валидация хэщтэга
 function validityHashtag(hashtag) {
   var re = /^#[a-zA-ZА-Яа-я0-9]*$/;
+  hashtag.toLowerCase();
   var inputArea = document.querySelector('.text__hashtags');
   for (var i = 0; i < hashtag.length; i++) {
     if (!re.test(hashtag[i])) {
@@ -165,16 +166,36 @@ function zoomer(valueZoomer) {
 }
 
 // загрузка фотографии
+
+var effect;
+var picture = document.querySelector('.img-upload__preview img');
+
 function upLoadPhoto(evt) {
   var upLoadModal = document.querySelector('.img-upload__overlay');
   var scrollInput = document.querySelector('.effect-level__pin');
   var zoomContorlSmiller = document.querySelector('.scale__control--smaller');
   var zoomControllBigger = document.querySelector('.scale__control--bigger');
   var startScroll;
+  var buttonClose = document.querySelector('.img-upload__cancel');
   var effectLevel = document.querySelector('.img-upload__effect-level');
-
+  function closeModal() {
+    upLoadModal.classList.add('hidden');
+    scrollInput.removeEventListener('mousedown', undefined);
+    buttonClose.removeEventListener('click', closeModal);
+  }
 
   if (evt.target.id === 'upload-file') {
+    scrollInput.addEventListener('mousedown', function pressMouse(evtevt) {
+      evtevt.preventDefault();
+      startScroll = evtevt.clientX;
+      effectLevel.addEventListener('mousemove', scrollMove);
+      effectLevel.addEventListener('mouseup', scrollUp);
+      effectLevel.addEventListener('mouseleave', scrollUp);
+      return startScroll;
+    });
+
+    buttonClose.addEventListener('click', closeModal);
+
     upLoadModal.classList.remove('hidden');
     var body = document.querySelector('body');
     body.classList.add('modal-open');
@@ -189,15 +210,6 @@ function upLoadPhoto(evt) {
   }
 
   // скролл фильтра
-
-  scrollInput.addEventListener('mousedown', function (evtevt) {
-    evtevt.preventDefault();
-    startScroll = evtevt.clientX;
-    effectLevel.addEventListener('mousemove', scrollMove);
-    effectLevel.addEventListener('mouseup', scrollUp);
-    effectLevel.addEventListener('mouseleave', scrollUp);
-    return startScroll;
-  });
 
   function scrollMove(moveEvt) {
     moveEvt.preventDefault();
@@ -215,7 +227,7 @@ function upLoadPhoto(evt) {
     depth.style.width = scrollPosition + 'px';
     scrollPosition = scrollPosition * 100 / 450;
     scrollInput.value = scrollPosition;
-    return scrollPosition;
+    return applyEffect(scrollPosition);
   }
 
   function scrollUp(upEvt) {
@@ -226,38 +238,86 @@ function upLoadPhoto(evt) {
 
   // применение фильтра
   if (evt.target.classList.contains('effects__radio')) {
-    var picture = document.querySelector('.img-upload__preview img');
-    var effect = evt.target.id;
-    if (effect === 'effect-chrome') {
-      picture.className = ('');
-      picture.classList.add('effects__preview--chrome');
-    }
-    if (effect === 'effect-sepia') {
-      picture.className = ('');
-      picture.classList.add('effects__preview--sepia');
-    }
-    if (effect === 'effect-marvin') {
-      picture.className = ('');
-      picture.classList.add('effects__preview--marvin');
-      picture.style = 'filter: invert(' + scrollMove + '%)';
-    }
-    if (effect === 'effect-phobos') {
-      picture.className = ('');
-      picture.classList.add('effects__preview--phobos');
-    }
-    if (effect === 'effect-heat') {
-      picture.className = ('');
-      picture.classList.add('effects__preview--heat');
-    }
-    if (effect === 'effect-none') {
-      picture.className = ('');
-    }
+    effect = evt.target.id;
+    applyEffect(100);
+    returnSlider();
   }
 
   if (evt.target.classList.contains('text__hashtags')) {
     var hashtag = document.querySelector('.text__hashtags').value;
     var hashtagArray = hashtag.split(' ');
     validityHashtag(hashtagArray);
+  }
+}
+// возвращение слайдера
+function returnSlider() {
+  var scrollInput = document.querySelector('.effect-level__pin');
+  var depth = document.querySelector('.effect-level__depth');
+  scrollInput.style = 'left:450px';
+  depth.style = 'width:450px';
+}
+
+function applyEffect(scrollMove) {
+  var effectLevel = document.querySelector('.img-upload__effect-level');
+  if (effect === 'effect-chrome') {
+    picture.className = ('');
+    effectLevel.classList.remove('hidden');
+    picture.classList.add('effects__preview--chrome');
+    picture.style = 'filter: grayscale(' + scrollMove / 100 + ')';
+
+  }
+  if (effect === 'effect-sepia') {
+    picture.className = ('');
+    effectLevel.classList.remove('hidden');
+    picture.classList.add('effects__preview--sepia');
+    effectLevel.classList.remove('hidden');
+    picture.style = 'filter: sepia(' + scrollMove / 100 + ')';
+  }
+  if (effect === 'effect-marvin') {
+    picture.className = ('');
+    picture.classList.add('effects__preview--marvin');
+    effectLevel.classList.remove('hidden');
+    picture.style = 'filter: invert(' + scrollMove + '%)';
+  }
+  if (effect === 'effect-phobos') {
+    picture.className = ('');
+    picture.classList.add('effects__preview--phobos');
+    effectLevel.classList.remove('hidden');
+    var phobosLevel;
+    if (scrollMove < 25) {
+      phobosLevel = 0;
+    }
+    if (scrollMove < 50 && scrollMove > 25) {
+      phobosLevel = 1;
+    }
+    if (scrollMove < 75 && scrollMove > 50) {
+      phobosLevel = 2;
+    }
+    if (scrollMove > 75) {
+      phobosLevel = 3;
+    }
+    picture.style = 'filter: blur(' + phobosLevel + 'px)';
+  }
+  if (effect === 'effect-heat') {
+    picture.className = ('');
+    picture.classList.add('effects__preview--heat');
+    effectLevel.classList.remove('hidden');
+    var heatLevel;
+    if (scrollMove < 33) {
+      heatLevel = 1;
+    }
+    if (scrollMove < 66 && scrollMove > 33) {
+      heatLevel = 2;
+    }
+    if (scrollMove > 66) {
+      heatLevel = 3;
+    }
+    picture.style = 'filter: brightness(' + heatLevel + ')';
+  }
+  if (effect === 'effect-none') {
+    picture.className = ('');
+    effectLevel.classList.add('hidden');
+    picture.style = '';
   }
 }
 
