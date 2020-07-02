552b011426796063089e281228bed4ead56e5d57
qstyle <ordin404@gmail.com>
2020-07-02 21:18:49 +0300

    add modal function

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function generateRandom(randomMin, randomMax) {
 
 // генерация сообщения коментария
 function generateMessage() {
-  var message = [];
+  var messages = [];
   var messageComent = [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
@@ -22,22 +22,22 @@ function generateMessage() {
   ];
   for (var i = 0; i < generateRandom(1, 2); i++) {
     var randomMessageIndex = generateRandom(0, 5);
-    message.push(messageComent[randomMessageIndex]);
+    messages.push(messageComent[randomMessageIndex]);
   }
-  return message.join('');
+  return messages.join('');
 }
 
 // генерация коментариев
 function generateComment() {
   var comment = {};
   var randomAvatar = [1, 2, 3, 4, 5, 6];
-  var nameComent = [
+  var nameComents = [
     'name1', 'name2', 'name3', 'name4', 'name5', 'name6'
   ];
   comment = {
     avatar: 'img/avatar-' + randomAvatar[generateRandom(0, 5)] + '.svg',
     message: generateMessage(),
-    name: nameComent[generateRandom(0, 5)]
+    name: nameComents[generateRandom(0, 5)]
   };
   return comment;
 }
@@ -48,12 +48,11 @@ function generateComments() {
     comments.push(generateComment());
   }
   return comments;
-
 }
 
 // гернерация фото
 function generateBlockPhoto(numberOfCopies) {
-  var photoArray = [];
+  var photosArray = [];
   for (var i = 0; i < numberOfCopies; i++) {
     var blockPhotoObj = {
       url: 'photos/' + (i + 1) + '.jpg',
@@ -61,14 +60,13 @@ function generateBlockPhoto(numberOfCopies) {
       likes: generateRandom(15, 250),
       comments: generateComments()
     };
-    photoArray.push(blockPhotoObj);
+    photosArray.push(blockPhotoObj);
   }
-  return photoArray;
+  return photosArray;
 }
 var blockPhoto = generateBlockPhoto(25);
 
 // создание блока фото
-
 var picturesContainer = document.querySelector('.pictures');
 var photoElementBlock = document.querySelector('#picture').content;
 var renderBlockPhoto = function (rednerBlock) {
@@ -126,34 +124,40 @@ function openBigPicture(photoObject) {
 
 
 // валидация хэщтэга
-function validityHashtag(hashtag) {
+function validiteHashtag(hashtags) {
+  for (var y = 0; y < hashtags.length; y++) {
+    hashtags[y] = hashtags[y].toLowerCase();
+  }
   var re = /^#[a-zA-ZА-Яа-я0-9]*$/;
-  hashtag.toLowerCase();
-  var inputArea = document.querySelector('.text__hashtags');
-  for (var i = 0; i < hashtag.length; i++) {
-    if (!re.test(hashtag[i])) {
-      inputArea.setCustomValidity('alarm alarm');
-    } else if (hashtag[i].length > 20 || hashtag[i].length < 2) {
-      inputArea.setCustomValidity('alarm alarm2');
-    } else if (!(hashtag.indexOf(hashtag[i]) === hashtag.lastIndexOf(hashtag[i]))) {
-      inputArea.setCustomValidity('alarm alarm3');
-    } else if (hashtag.length > 5) {
-      inputArea.setCustomValidity('alarm alarm4');
+  for (var i = 0; i < hashtags.length; i++) {
+    var validStatus = 0;
+    if (!re.test(hashtags[i])) {
+      validStatus = 1;
+      return validStatus;
+    } else if (hashtags[i].length > 20 || hashtags[i].length < 2) {
+      validStatus = 2;
+      return validStatus;
+    } else if (!(hashtags.indexOf(hashtags[i]) === hashtags.lastIndexOf(hashtags[i]))) {
+      validStatus = 3;
+      return validStatus;
+    } else if (hashtags.length > 5) {
+      validStatus = 4;
+      return validStatus;
+    } else {
+      validStatus = 0;
+      return validStatus;
     }
   }
-
+  return validStatus;
 }
-
 // увеличение и уменьшение картинки в редакторе
 
 function appZoomer(value) {
-  var pictureUpLoad = document.querySelector('.img-upload__preview img');
   document.querySelector('.scale__control--value').value = value + '%';
-  pictureUpLoad.style = 'transform: scale(' + value / 100 + ')';
+  picture.style = 'transform: scale(' + value / 100 + ')';
 }
 
-
-function zoomer(valueZoomer) {
+function zoomerPhoto(valueZoomer) {
   var value = parseFloat(document.querySelector('.scale__control--value').value);
   value = value + valueZoomer * 25;
   if (value >= 100) {
@@ -166,165 +170,160 @@ function zoomer(valueZoomer) {
   return value;
 }
 
-// загрузка фотографии
-
-var effect;
+// scroll
+var startScroll;
+var effectLevel = document.querySelector('.img-upload__effect-level');
+var scrollInput = document.querySelector('.effect-level__pin');
+var upLoadModal = document.querySelector('.img-upload__overlay');
+var buttonClose = document.querySelector('.img-upload__cancel');
 var picture = document.querySelector('.img-upload__preview img');
+var inputWidth;
+var body = document.querySelector('body');
+function pressMouse(evtevt) {
+  evtevt.preventDefault();
+  startScroll = evtevt.clientX;
+  effectLevel.addEventListener('mousemove', scrollMove);
+  effectLevel.addEventListener('mouseup', scrollUp);
+  effectLevel.addEventListener('mouseleave', scrollUp);
+  return startScroll;
+}
 
+function scrollUp(upEvt) {
+  upEvt.preventDefault();
+  effectLevel.removeEventListener('mousemove', scrollMove);
+  effectLevel.removeEventListener('mouseup', scrollUp);
+}
+
+function closeModal() {
+  upLoadModal.classList.add('hidden');
+  scrollInput.removeEventListener('mousedown', undefined);
+  buttonClose.removeEventListener('click', closeModal);
+  body.classList.remove('modal-open');
+}
+
+// загрузка фотографии
+var effect;
 function upLoadPhoto(evt) {
-  var upLoadModal = document.querySelector('.img-upload__overlay');
-  var scrollInput = document.querySelector('.effect-level__pin');
   var zoomContorlSmiller = document.querySelector('.scale__control--smaller');
   var zoomControllBigger = document.querySelector('.scale__control--bigger');
-  var startScroll;
-  var buttonClose = document.querySelector('.img-upload__cancel');
-  var effectLevel = document.querySelector('.img-upload__effect-level');
-  function closeModal() {
-    upLoadModal.classList.add('hidden');
-    scrollInput.removeEventListener('mousedown', undefined);
-    buttonClose.removeEventListener('click', closeModal);
-  }
-
   if (evt.target.id === 'upload-file') {
-    scrollInput.addEventListener('mousedown', function pressMouse(evtevt) {
-      evtevt.preventDefault();
-      startScroll = evtevt.clientX;
-      effectLevel.addEventListener('mousemove', scrollMove);
-      effectLevel.addEventListener('mouseup', scrollUp);
-      effectLevel.addEventListener('mouseleave', scrollUp);
-      return startScroll;
-    });
-
+    scrollInput.addEventListener('mousedown', pressMouse);
     buttonClose.addEventListener('click', closeModal);
-
     upLoadModal.classList.remove('hidden');
-    var body = document.querySelector('body');
     body.classList.add('modal-open');
     zoomContorlSmiller.addEventListener('click', function zoomSmiller() {
-      var valueZoomer = -1;
-      return zoomer(valueZoomer);
+      zoomerPhoto(-1);
     });
     zoomControllBigger.addEventListener('click', function zoomBigger() {
-      var valueZoomer = 1;
-      zoomer(valueZoomer);
+      zoomerPhoto(1);
     });
-  }
-
-  // скролл фильтра
-
-  function scrollMove(moveEvt) {
-    moveEvt.preventDefault();
-    var move = startScroll - moveEvt.clientX;
-    startScroll = moveEvt.clientX;
-    var scrollPosition = scrollInput.offsetLeft - move;
-    if (scrollPosition < 0) {
-      scrollPosition = 0;
-    }
-    if (scrollPosition > 450) {
-      scrollPosition = 450;
-    }
-    var depth = document.querySelector('.effect-level__depth');
-    scrollInput.style.left = scrollPosition + 'px';
-    depth.style.width = scrollPosition + 'px';
-    scrollPosition = scrollPosition * 100 / 450;
-    scrollInput.value = scrollPosition;
-    return applyEffect(scrollPosition);
-  }
-
-  function scrollUp(upEvt) {
-    upEvt.preventDefault();
-    effectLevel.removeEventListener('mousemove', scrollMove);
-    effectLevel.removeEventListener('mouseup', scrollUp);
   }
 
   // применение фильтра
   if (evt.target.classList.contains('effects__radio')) {
-    effect = evt.target.id;
+    effect = evt.target.value;
     applyEffect(100);
-    returnSlider();
+    resetSlider();
   }
 
+  // валидация хэщтэга
   if (evt.target.classList.contains('text__hashtags')) {
-    var hashtag = document.querySelector('.text__hashtags').value;
-    var hashtagArray = hashtag.split(' ');
-    validityHashtag(hashtagArray);
+    var hashtags = document.querySelector('.text__hashtags').value;
+    var hashtagArray = hashtags.split(' ');
+    var inputArea = document.querySelector('.text__hashtags');
+    if ((hashtagArray.length === 1 && hashtagArray[0] === '')) {
+      inputArea.setCustomValidity('');
+    } else {
+      var hashtagValidity = validiteHashtag(hashtagArray);
+      if (hashtagValidity === 0) {
+        inputArea.style = 'background-color: green';
+        inputArea.setCustomValidity('');
+      }
+      if (validiteHashtag(hashtagArray) === 1) {
+        inputArea.style = 'background-color: white';
+        inputArea.setCustomValidity('строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.');
+      }
+      if (validiteHashtag(hashtagArray) === 2) {
+        inputArea.style = 'background-color: white';
+        inputArea.setCustomValidity('хеш-тег не может состоять только из одной решётки, максимальная длина одного хэш-тега 20 символов, включая решётку');
+      }
+      if (validiteHashtag(hashtagArray) === 3) {
+        inputArea.style = 'background-color: white';
+        inputArea.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');
+      }
+      if (validiteHashtag(hashtagArray) === 4) {
+        inputArea.style = 'background-color: white';
+        inputArea.setCustomValidity('нельзя указать больше пяти хэш-тегов');
+      }
+    }
   }
 }
+
 // возвращение слайдера
-function returnSlider() {
-  var scrollInput = document.querySelector('.effect-level__pin');
+function resetSlider() {
   var depth = document.querySelector('.effect-level__depth');
-  scrollInput.style = 'left:450px';
-  depth.style = 'width:450px';
+  scrollInput.style = 'left:' + inputWidth + '';
+  depth.style = 'width:' + inputWidth + '';
 }
 
-function applyEffect(scrollMove) {
-  var effectLevel = document.querySelector('.img-upload__effect-level');
-  if (effect === 'effect-chrome') {
-    picture.className = ('');
-    effectLevel.classList.remove('hidden');
-    picture.classList.add('effects__preview--chrome');
-    picture.style = 'filter: grayscale(' + scrollMove / 100 + ')';
+function applyEffect(scrolMove) {
+  picture.className = ('');
+  effectLevel.classList.remove('hidden');
+  switch (effect) {
+    case 'chrome':
+      picture.classList.add('effects__preview--chrome');
+      picture.style = 'filter: grayscale(' + scrolMove / 100 + ')';
+      break;
+    case 'sepia':
+      picture.classList.add('effects__preview--sepia');
+      picture.style = 'filter: sepia(' + scrolMove / 100 + ')';
+      break;
+    case 'marvin':
+      picture.classList.add('effects__preview--marvin');
+      picture.style = 'filter: invert(' + scrolMove + '%)';
+      break;
+    case 'phobos':
+      picture.classList.add('effects__preview--phobos');
+      var phobosLevel = scrolMove * 3 / 100;
+      picture.style = 'filter: blur(' + phobosLevel + 'px)';
+      break;
+    case 'heat':
+      var heatLevel = scrolMove * 3 / 100;
+      if (heatLevel < 1) {
+        heatLevel = 1;
+      }
+      picture.classList.add('effects__preview--heat');
+      picture.style = 'filter: brightness(' + heatLevel + ')';
+      break;
+    case 'none':
+      effectLevel.classList.add('hidden');
+      picture.style = '';
+      break;
+  }
+}
 
+function scrollMove(moveEvt) {
+  moveEvt.preventDefault();
+  var move = startScroll - moveEvt.clientX;
+  inputWidth = document.querySelector('.effect-level__line').offsetWidth;
+  startScroll = moveEvt.clientX;
+  var scrollPosition = scrollInput.offsetLeft - move;
+  if (scrollPosition < 0) {
+    scrollPosition = 0;
   }
-  if (effect === 'effect-sepia') {
-    picture.className = ('');
-    effectLevel.classList.remove('hidden');
-    picture.classList.add('effects__preview--sepia');
-    effectLevel.classList.remove('hidden');
-    picture.style = 'filter: sepia(' + scrollMove / 100 + ')';
+  if (scrollPosition > inputWidth) {
+    scrollPosition = inputWidth;
   }
-  if (effect === 'effect-marvin') {
-    picture.className = ('');
-    picture.classList.add('effects__preview--marvin');
-    effectLevel.classList.remove('hidden');
-    picture.style = 'filter: invert(' + scrollMove + '%)';
-  }
-  if (effect === 'effect-phobos') {
-    picture.className = ('');
-    picture.classList.add('effects__preview--phobos');
-    effectLevel.classList.remove('hidden');
-    var phobosLevel;
-    if (scrollMove < 25) {
-      phobosLevel = 0;
-    }
-    if (scrollMove < 50 && scrollMove > 25) {
-      phobosLevel = 1;
-    }
-    if (scrollMove < 75 && scrollMove > 50) {
-      phobosLevel = 2;
-    }
-    if (scrollMove > 75) {
-      phobosLevel = 3;
-    }
-    picture.style = 'filter: blur(' + phobosLevel + 'px)';
-  }
-  if (effect === 'effect-heat') {
-    picture.className = ('');
-    picture.classList.add('effects__preview--heat');
-    effectLevel.classList.remove('hidden');
-    var heatLevel;
-    if (scrollMove < 33) {
-      heatLevel = 1;
-    }
-    if (scrollMove < 66 && scrollMove > 33) {
-      heatLevel = 2;
-    }
-    if (scrollMove > 66) {
-      heatLevel = 3;
-    }
-    picture.style = 'filter: brightness(' + heatLevel + ')';
-  }
-  if (effect === 'effect-none') {
-    picture.className = ('');
-    effectLevel.classList.add('hidden');
-    picture.style = '';
-  }
+  var depth = document.querySelector('.effect-level__depth');
+  scrollInput.style.left = scrollPosition + 'px';
+  depth.style.width = scrollPosition + 'px';
+  scrollPosition = scrollPosition * 100 / inputWidth;
+  scrollInput.value = scrollPosition;
+  return applyEffect(scrollPosition);
 }
 
 var upLoadForm = document.querySelector('#upload-select-image');
 upLoadForm.addEventListener('change', function upLoadEvent(evt) {
-  evt.preventDefault();
   upLoadPhoto(evt);
 });
 
