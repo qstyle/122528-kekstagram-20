b64453b113e276469e847481dd2a0c6c5be22eaa
qstyle <ordin404@gmail.com>
2020-07-13 20:35:10 +0300

    load form

--- js/submitModalForm.js
+++ js/submitModalForm.js
@@ -4,37 +4,41 @@
   var successModal = (document.querySelector('#success').content).cloneNode(true);
   window.successForm = function () {
     main.appendChild(successModal);
-    document.addEventListener('click', closeSuccessForm());
-    document.addEventListener('keydown', function closeSuccess(evt) {
-      if (evt.keyCode === 27) {
-        closeSuccessForm();
-      }
-    });
+    document.addEventListener('click', closeSuccessForm);
+    document.addEventListener('keydown', closeSuccess);
   };
+
+  function closeSuccess(evt) {
+    if (evt.keyCode === 27) {
+      closeSuccessForm();
+    }
+  }
 
   window.errorForm = function () {
     var errorModal = (document.querySelector('#error').content).cloneNode(true);
     main.appendChild(errorModal);
     document.addEventListener('click', closeErrorForm);
-    document.addEventListener('keydown', function closeError(evt) {
-      if (evt.keyCode === 27) {
-        closeErrorForm();
-      }
-    });
+    document.addEventListener('keydown', closeError);
   };
+
+  function closeError(evt) {
+    if (evt.keyCode === 27) {
+      closeErrorForm();
+    }
+  }
 
   function closeSuccessForm() {
     var successModalCloser = main.querySelector('.success');
     main.removeChild(successModalCloser);
     document.removeEventListener('click', closeSuccessForm);
-    document.addEventListener('keydown', undefined);
+    document.removeEventListener('keydown', closeSuccess);
   }
 
   function closeErrorForm() {
     var errorModalCloser = main.querySelector('.error');
     main.removeChild(errorModalCloser);
     document.removeEventListener('click', closeErrorForm);
-    document.addEventListener('keydown', undefined);
+    document.removeEventListener('keydown', closeError);
   }
 }
 )();
