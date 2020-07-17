c18344266c13257c9c1a2d24798abf0cad297cf0
qstyle <ordin404@gmail.com>
2020-07-18 00:17:12 +0300

    fix(js)filter

--- js/applyFilter.js
+++ js/applyFilter.js
@@ -1,48 +1,75 @@
 'use strict';
 (function () {
   var filterBar = document.querySelector('.img-filters');
+  var buttonFilter = document.querySelectorAll('.img-filters__button');
   var buttonFilterDefault = document.querySelector('#filter-default');
   var buttonFilterRandom = document.querySelector('#filter-random');
   var buttonFilterDiscussed = document.querySelector('#filter-discussed');
+
   window.renderFilterBar = function () {
     filterBar.classList.remove('img-filters--inactive');
   };
 
-  buttonFilterDefault.addEventListener('click', function applayDefaultFilter() {
-    window.addPhotoPage(window.loadData, 1);
-    buttonFilterDefault.classList.add('img-filters__button--active');
-    buttonFilterRandom.classList.remove('img-filters__button--active');
-    buttonFilterDiscussed.classList.remove('img-filters__button--active');
-  });
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function (evt) {
+      var context = evt;
+      var args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) {
+        func.apply(context, args);
+      }
+    };
+  }
 
-  buttonFilterRandom.addEventListener('click', function applayRandomFilter() {
-    generateRandomFilter();
-    buttonFilterDefault.classList.remove('img-filters__button--active');
-    buttonFilterRandom.classList.add('img-filters__button--active');
-    buttonFilterDiscussed.classList.remove('img-filters__button--active');
-  });
+  for (var i = 0; i < buttonFilter.length; i++) {
+    buttonFilter[i].addEventListener('click', debounce(filterHandler, 500, 0));
+  }
 
-  buttonFilterDiscussed.addEventListener('click', function applayDiscussedFilter() {
-    generateDiscussedFilter();
-    buttonFilterDefault.classList.remove('img-filters__button--active');
-    buttonFilterRandom.classList.remove('img-filters__button--active');
-    buttonFilterDiscussed.classList.add('img-filters__button--active');
-  });
+
+  function filterHandler(evt) {
+    if (evt.target.id === buttonFilterDefault.id) {
+      window.addPhotoPage(window.loadData, 1);
+      buttonFilterDefault.classList.add('img-filters__button--active');
+      buttonFilterRandom.classList.remove('img-filters__button--active');
+      buttonFilterDiscussed.classList.remove('img-filters__button--active');
+    }
+    if (evt.target.id === buttonFilterRandom.id) {
+      generateRandomFilter();
+      buttonFilterDefault.classList.remove('img-filters__button--active');
+      buttonFilterRandom.classList.add('img-filters__button--active');
+      buttonFilterDiscussed.classList.remove('img-filters__button--active');
+    }
+    if (evt.target.id === buttonFilterDiscussed.id) {
+      generateDiscussedFilter();
+      buttonFilterDefault.classList.remove('img-filters__button--active');
+      buttonFilterRandom.classList.remove('img-filters__button--active');
+      buttonFilterDiscussed.classList.add('img-filters__button--active');
+    }
+  }
 
   function generateRandomFilter() {
     var randomPhotoArraysIndex = [];
-    for (var i = 0; randomPhotoArraysIndex.length < 10; i++) {
+    for (var j = 0; randomPhotoArraysIndex.length < 10; j++) {
       var randomPhotoIndex = window.generateRandom(0, window.loadData.length);
       if (!(randomPhotoArraysIndex.includes(randomPhotoIndex))) {
         randomPhotoArraysIndex.push(randomPhotoIndex);
       }
     }
-    var randomPhotoArrays = [];
-    for (var j = 0; j < randomPhotoArraysIndex.length; j++) {
-      randomPhotoArrays.push(window.loadData[randomPhotoArraysIndex[j]]);
-    }
+    var randomPhotoArrays = randomPhotoArraysIndex.map(function (element) {
+      return window.loadData[element];
+    });
     window.addPhotoPage(randomPhotoArrays, 1);
   }
+
   function generateDiscussedFilter() {
     var discussedArray = window.loadData;
     discussedArray.sort(function (a, b) {
